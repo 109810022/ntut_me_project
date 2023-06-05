519c0e3139aa9aea58bb3b2e11c09e045c75f0b3
109810022 <100888502+109810022@users.noreply.github.com>
2023-06-05 16:11:49 +0800

    Update v1_with_yolo.py

--- aiclass_project/v1_with_yolo.py
+++ aiclass_project/v1_with_yolo.py
@@ -14,7 +14,7 @@ config = rs.config()    # 定义配置config
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 15)      # 配置depth流
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)     # 配置color流
 # Load the YOLOv8 model
-model = YOLO('yolov8n.pt')
+model = YOLO('yolov8n-pose.pt')
 
 pipe_profile = pipeline.start(config)       # streaming流开始
 
@@ -64,7 +64,7 @@ if __name__=="__main__":
         获取对齐图像帧与相机参数
         '''
         color_intrin, depth_intrin, img_color, img_depth, aligned_depth_frame = get_aligned_images()        # 获取对齐图像与相机参数
-        results = model(img_color,conf=0.7)
+        results = model.track(img_color,conf=0.7,)
         img_color = results[0].plot()
 
 
@@ -74,6 +74,16 @@ if __name__=="__main__":
         获取随机点三维坐标
         '''
         boxes = results[0].boxes
+        try:
+            left_shoulder = results[0].keypoints.xy[0][5]
+            right_shoulder = results[0].keypoints.xy[0][6]
+            position = [int((left_shoulder[0]+right_shoulder[0])/2),int((left_shoulder[1]+right_shoulder[1])/2)]
+            print(position)
+
+        except:
+            position = [0,0]
+            pass
+
         coordinate = boxes.xyxy.numpy()
         
         try:
@@ -82,7 +92,7 @@ if __name__=="__main__":
         except:
             depth_pixel = [320, 240]
 
-        dis, camera_coordinate = get_3d_camera_coordinate(depth_pixel, aligned_depth_frame, depth_intrin)
+        dis, camera_coordinate = get_3d_camera_coordinate(position, aligned_depth_frame, depth_intrin)
         print ('depth: ',dis)       # 深度单位是mm
         print ('camera_coordinate: ',camera_coordinate)
 
@@ -91,12 +101,13 @@ if __name__=="__main__":
         显示图像与标注
         '''
         #### 在图中标记随机点及其坐标 ####
-        cv2.circle(img_color, depth_pixel, 8, [255,0,255], thickness=-1)
-        cv2.putText(img_color,"Dis:"+str(dis)+" m", (40,40), cv2.FONT_HERSHEY_SIMPLEX, 1.2,[0,0,255])
-        cv2.putText(img_color,"X:"+str(camera_coordinate[0])+" m", (80,80), cv2.FONT_HERSHEY_SIMPLEX, 1.2,[255,0,0])
-        cv2.putText(img_color,"Y:"+str(camera_coordinate[1])+" m", (80,120), cv2.FONT_HERSHEY_SIMPLEX, 1.2,[255,0,0])
-        cv2.putText(img_color,"Z:"+str(camera_coordinate[2])+" m", (80,160), cv2.FONT_HERSHEY_SIMPLEX, 1.2,[255,0,0])
+        cv2.circle(img_color, position, 8, [255,0,255], thickness=-1)
+        cv2.putText(img_color,"Dis:"+str(dis)+" m", (40,40), cv2.FONT_HERSHEY_COMPLEX, 1.,[0,0,255])
+        cv2.putText(img_color,"X:"+str(camera_coordinate[0])+" m", (80,80), cv2.FONT_HERSHEY_COMPLEX, 1.,[255,0,0])
+        cv2.putText(img_color,"Y:"+str(camera_coordinate[1])+" m", (80,120), cv2.FONT_HERSHEY_COMPLEX, 1.,[255,0,0])
+        cv2.putText(img_color,"Z:"+str(camera_coordinate[2])+" m",  depth_pixel, cv2.FONT_HERSHEY_COMPLEX, 1.,[255,0,0])
         
         #### 显示画面 ####
         cv2.imshow('RealSence',img_color)
         key = cv2.waitKey(1)
+
