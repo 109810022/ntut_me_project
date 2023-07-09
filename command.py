9b9dacda80127c2513e75254af758ff35500b2c3
cchhii0917 <100888502+109810022@users.noreply.github.com>
2023-07-09 15:54:35 +0800

    Update command.py

--- command.py
+++ command.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import pyrealsense2 as rs
 import numpy as np
 import cv2
@@ -5,8 +6,8 @@ from ultralytics import YOLO
 import torch
 import numpy as np
 import math 
-#import rospy
-#from geometry_msgs.msg import Twist 
+import rospy
+from geometry_msgs.msg import Twist 
 
 
 
@@ -25,29 +26,37 @@ pipe_profile = pipeline.start(config)       # streaming流开始
 align_to = rs.stream.color      # align_to 是计划对齐深度帧的流类型
 align = rs.align(align_to)      # rs.align 执行深度帧与其他帧的对齐
 
-'''for ros
+
 puber = "/twist_cmd" 
 pub = rospy.Publisher(puber, Twist, queue_size=10)
 rospy.init_node('talker', anonymous=True)
-'''
+
 
 
 '''
 函式庫 ##########################################################
 '''
 def velocity(dis):
-    kp = 1
-    safe_dis = 2 
+    kp = 0.5
+    safe_dis = 1
     vel = kp * (dis-safe_dis)
+    return vel 
+    '''
     if vel < 0 : return 0
+    elif vel >5: return 5
     else : return vel  
+    '''
+'''
+
 def angle_rotation(angle): #80度 切成8等分
     ang = angle//10
-    if ang > 4 :
-        return 40*3.14159/180
-    elif ang < -4:
-        return -40*3.14159/180 
-    else: return ang*10*3.14159/180
+    if ang > 3 :
+        return -30*3.14159/180
+    elif ang < -3:
+        return 30*3.14159/180 
+    else: return -ang*10*3.14159/180
+'''
+'''
 def motion(vel,ang):        
     return {"linear":     
             {"x": 0.0, #vel  
@@ -60,15 +69,25 @@ def motion(vel,ang):
             }    
 '''
 def motion(x_vel,z_ang):
-	cm = Twist()
-	cm.linear.x = 0
-	cm.linear.y = 0
-	cm.linear.z = 0
-	cm.angular.x = 0
-	cm.angular.y = 0
-	cm.angular.z = z_ang
-	return cm
-'''
+    if x_vel >=0:
+        cm = Twist()
+        cm.linear.x = x_vel
+        cm.linear.y = 0
+        cm.linear.z = 0
+        cm.angular.x = 0
+        cm.angular.y = 0
+        cm.angular.z = -z_ang
+        return cm
+    else :
+        cm = Twist()
+        cm.linear.x = x_vel
+        cm.linear.y = 0
+        cm.linear.z = 0
+        cm.angular.x = 0
+        cm.angular.y = 0
+        cm.angular.z = z_ang
+        return cm
+
 ''' 
 获取对齐图像帧与相机参数
 '''
@@ -120,21 +139,21 @@ if __name__=="__main__":
         #print(boxes)
         try:
             
-            left_shoulder = boxes.xy[0][5]
+            left_shoulder = boxes[0][5]
             
-            right_shoulder = boxes.xy[0][6]
+            right_shoulder = boxes[0][6]
            
             position = [int((left_shoulder[0]+right_shoulder[0])/2),int((left_shoulder[1]+right_shoulder[1])/2)]
             
-            print("圖片二維座標:",position)
+            #print("圖片二維座標:",position)
     
             dis, camera_coordinate = get_3d_camera_coordinate(position, aligned_depth_frame, depth_intrin)
-            print ('距離: ',dis)       # 深度单位是mm
-            print("速度:",velocity(dis))
-            print ('空間實際座標: ',camera_coordinate)
+            #print ('距離: ',dis)       # 深度单位是mm
+            #print("速度:",velocity(dis))
+            #print ('空間實際座標: ',camera_coordinate)
             move = motion(velocity(dis),math.atan(camera_coordinate[0]/camera_coordinate[2]))
-            print("發布給車子的命令:",move)
-            #rospy.publish(move)
+            #print("發布給車子的命令:",move)
+            pub.publish(move)
 
             ''' 
             显示图像与标注
@@ -146,7 +165,7 @@ if __name__=="__main__":
             cv2.putText(img_color,"Y:"+str(camera_coordinate[1])+" m", (80,120), cv2.FONT_HERSHEY_COMPLEX, 1.,[255,0,0])
             cv2.putText(img_color,"Z:"+str(camera_coordinate[2])+" m",  position, cv2.FONT_HERSHEY_COMPLEX, 1.,[255,0,0])
 
-            #rospy.loginfo(motion(camera_coordinate[2],math.atan(camera_coordinate[0]/camera_coordinate[2])))
+            rospy.loginfo(motion(camera_coordinate[2],math.atan(camera_coordinate[0]/camera_coordinate[2])))
             
 
         except Exception as e: print(e)
